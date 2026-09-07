064591ee5252d093852431d276226c8c6f99014a
maximemathon38-afk <maximemathon38@gmail.com>
2026-09-07 16:21:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 window.RBF_CONFIG = {
   SUPABASE_URL: "https://vlvbngmzbftnwufrpish.supabase.co",
-
-  SUPABASE_ANON_KEY: "sb_secret_WpPI2VvJ5I8An2iPrFaurw_8gtwM5Za",
-
+  SUPABASE_ANON_KEY: "sb_publishable_H19cTagCVUOAw3o-J42iCw_RO8GDWS_",
   ADMIN_CODE: "RBF2026",
-
   REFRESH_INTERVAL_MS: 6000
 };
