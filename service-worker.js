7f41668d662df084ab577a8e9818a137769f73b5
maximemathon38-afk <maximemathon38@gmail.com>
2026-09-07 16:04:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
 const CACHE="rbf-containers-v3-shell-1";
-const ASSETS=["./","./index.html","./styles.css","./config.js","./db.js","./app.js","./manifest.webmanifest","./assets/rbf-logo.png","./assets/rbf-banner.png","./assets/icon-192.png","./assets/icon-512.png"];
+const ASSETS=["./","./index.html","./styles.css","./config.js","./db.js","./app.js","./manifest.webmanifest","./rbf-logo.png","./rbf-banner.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>{
